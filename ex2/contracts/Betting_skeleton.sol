053ead7ddecc05c5ddbcda8d90833f844976ab3f
Jeremiah Andrews <jlandrews@berkeley.edu>
2017-09-21 18:58:15 -0700

    Enabled variable numbers of gamblers, with proportional payout.

--- ex2/contracts/Betting_skeleton.sol
+++ ex2/contracts/Betting_skeleton.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.4.15;
 contract BettingContract {
 	/* Standard state variables */
 	address owner;
-	address public gamblerA;
-	address public gamblerB;
 	address public oracle;
 	uint numBets;
+	uint maxBets = 10;
+	address[] gamblers = new address[](maxBets);
+	uint totalBet;
 	uint[] outcomes;
 
 	/* Structs are custom data structures with self-defined parameters */
@@ -30,11 +31,8 @@ contract BettingContract {
 	modifier OracleOnly() {require(msg.sender == oracle); _;}
 	modifier NotOracle() {require(msg.sender != oracle); _;}
 	modifier NotOwner() {require(msg.sender != owner); _;}
-	modifier NotAlreadyBet() {
-		require(msg.sender != gamblerA && msg.sender != gamblerB);
-		_;
-	}
-	modifier OpenBettingSlot() {require(numBets < 2); _;}
+	modifier NotAlreadyBet() {require(!bets[msg.sender].initialized); _;}
+	modifier OpenBettingSlot() {require(numBets < maxBets); _;}
 	modifier HasFunds(uint withdrawAmount) {
 		require(winnings[msg.sender] >= withdrawAmount);
 		_;
@@ -48,7 +46,7 @@ contract BettingContract {
 
 	/* Owner chooses their trusted Oracle */
 	function chooseOracle(address _oracle) public OwnerOnly() returns (address) {
-		if (_oracle == gamblerA || _oracle == gamblerB) revert();
+		if (bets[_oracle].initialized) revert();
 		oracle = _oracle;
 		return oracle;
 	}
@@ -62,28 +60,34 @@ contract BettingContract {
 	OpenBettingSlot()
 	NotAlreadyBet() returns (bool) {
 		bets[msg.sender] = Bet(_outcome, msg.value, true);
-		if (numBets==0)
-		  gamblerA = msg.sender;
-		else
-			gamblerB = msg.sender;
-		BetMade(msg.sender);
+		gamblers[numBets] = msg.sender;
+		totalBet += msg.value;
 		numBets += 1;
+		BetMade(msg.sender);
 		return true;
 	}
 
 	/* The oracle chooses which outcome wins */
 	function makeDecision(uint _outcome) public OracleOnly() {
-		if (bets[gamblerA].outcome == _outcome &&
-			  bets[gamblerB].outcome == _outcome) {
-			winnings[gamblerA] += bets[gamblerA].amount;
-			winnings[gamblerB] += bets[gamblerB].amount;
-		} else if (bets[gamblerA].outcome == _outcome) {
-			winnings[gamblerA] += bets[gamblerA].amount + bets[gamblerB].amount;
-		} else if (bets[gamblerB].outcome == _outcome) {
-			winnings[gamblerB] += bets[gamblerA].amount + bets[gamblerB].amount;
-		} else {
-			winnings[oracle] += bets[gamblerA].amount + bets[gamblerB].amount;
+		uint numWinners = 0;
+		uint winnerBets = 0;
+		for (uint i=0; i<numBets; i++) {
+			if (bets[gamblers[i]].outcome == _outcome) {
+				numWinners++;
+				winnerBets += bets[gamblers[i]].amount;
+			}
 		}
+		if (numWinners == 0) winnings[oracle] += totalBet;
+		else {
+			for (i=0; i<numBets; i++) {
+				if (bets[gamblers[i]].outcome == _outcome) {
+					//Each winner receives share of total bets
+					//proportional to their share of the winning bets
+					winnings[gamblers[i]] += (totalBet*bets[gamblers[i]].amount)/winnerBets;
+				}
+			}
+		}
+		contractReset();
 		BetClosed();
 	}
 
@@ -109,8 +113,10 @@ contract BettingContract {
 
 	/* Call delete() to reset certain state variables. Which ones? That's upto you to decide */
 	function contractReset() private {
-	    delete(gamblerA);
-	    delete(gamblerB);
+			for (uint i = 0; i < numBets; i++) {
+				delete(bets[gamblers[i]]);
+			}
+			delete(gamblers);
 	    numBets = 0;
 	}
 
